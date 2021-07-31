02548481c8a750c1b620f68164f3ff672dcc74e2
Felender <felender7@gmail.com>
2021-07-31 20:12:32 +0200

    added media url

--- api/models.py
+++ api/models.py
@@ -5,5 +5,5 @@ class Geeks(models.Model):
     full_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=11)
     role = models.CharField(max_length=100, null=True, blank=True)
-    profile_pic = models.ImageField(upload_to="photos", null=True, blank=True)
+    profile_pic = models.ImageField(upload_to="https://icedrive.net/1/05umfBtS3a", null=True, blank=True)
 
